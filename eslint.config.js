30e1ef2b6bf8293454363f8ec61e59b66618a95b
Peter Macdonald <macdonald.peter90@gmail.com>
2025-03-19 08:54:02 +0100

    Repo stuff, add contributors to footnotes, try something new with user names
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,40 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.yarn/*', 'node_modules', 'coverage', '.vscode', '*.env'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      'react': react,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...tseslint.configs.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
